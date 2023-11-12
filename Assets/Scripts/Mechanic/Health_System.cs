62b879eb512c44cedd39e3a40fada4f57ee6174e
Muhammad Zidan <79797943+Funikz@users.noreply.github.com>
2023-11-12 22:42:17 +0700

    heart_assets

--- Assets/Scripts/Mechanic/Health_System.cs
+++ Assets/Scripts/Mechanic/Health_System.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
-    public int health = 2; // Jumlah nyawa awal
+    public int health; // Jumlah nyawa awal
     public int numOfHearts;
 
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update() {
+        for (int i = 0; i < hearts.Length; i++) {
+            if (i < health) {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts) {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy")) // Gantilah "Enemy" dengan tag yang sesuai untuk musuh Anda
