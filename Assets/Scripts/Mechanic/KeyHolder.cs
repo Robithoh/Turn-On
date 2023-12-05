5c6a94e7a0b41d4075130dd4b2b0ee51e2691ded
Muhammad Zidan <79797943+Funikz@users.noreply.github.com>
2023-12-05 22:41:07 +0700

    open_door_withkey

--- Assets/Scripts/Mechanic/KeyHolder.cs
+++ Assets/Scripts/Mechanic/KeyHolder.cs
@@ -8,6 +8,7 @@ public class KeyHolder : MonoBehaviour
     private List<Key.KeyType> keyList;
     public Text keyNotification;
     bool isPlayerNear = false;
+    private KeyDoor currentKeyDoor; // Menyimpan referensi ke pintu yang sedang berdekatan
 
     private void Awake()
     {
@@ -34,7 +35,7 @@ public class KeyHolder : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //isPlayerNear = true;
+        isPlayerNear = true;
         Key key = other.GetComponent<Key>();
         if (key != null)
         {
@@ -45,10 +46,9 @@ public class KeyHolder : MonoBehaviour
         KeyDoor keyDoor = other.GetComponent<KeyDoor>();
         if (keyDoor != null)
         {
+            currentKeyDoor = keyDoor; // Simpan referensi ke pintu yang berdekatan
             if (ContainsKey(keyDoor.GetKeyType()))
             {
-                RemoveKey(keyDoor.GetKeyType());
-                keyDoor.ToggleDoor();
                 keyDoor.doorInfoFalse();
                 Destroy(other);
             }
@@ -59,27 +59,39 @@ public class KeyHolder : MonoBehaviour
         }
     }
 
-    //private void Update()
-    //{
-    //    if(isPlayerNear)
-    //    {
-    //        if(Input.GetKeyDown(KeyCode.E))
-    //        {
-    //            checkKey();
-    //            checkDoor();
-    //        }
-    //    }
-    //}
+    private void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            CheckDoor();
+        }
+    }
 
     private void OnTriggerExit(Collider other)
     {
         isPlayerNear = false;
         KeyDoor keyDoor = other.GetComponent<KeyDoor>();
         keyDoor.doorInfoFalse();
+        currentKeyDoor = null; // Atur kembali referensi ke pintu menjadi null
     }
 
     void HideKeyInfo()
     {
         keyNotification.text = "";
+    }
+
+    void CheckDoor()
+    {
+        if (currentKeyDoor != null && !ContainsKey(currentKeyDoor.GetKeyType()))
+        {
+            // Jika pintu berdekatan dan pemain tidak memiliki kunci yang sesuai, tidak lakukan apa-apa
+            return;
+        }
+
+        if (currentKeyDoor != null)
+        {
+            currentKeyDoor.ToggleDoor(); // Panggil metode untuk membuka pintu
+            RemoveKey(currentKeyDoor.GetKeyType()); // Hapus kunci dari daftar pemain
+        }
     }
 }
